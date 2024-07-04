a62db61ffa6aff76492c97171cc9570641250a5d
Cong Dinh <congdinh2012@hotmail.com>
2024-07-04 16:06:29 +0700

    Problem01: Complete C# solution

---
+++ Problem01/CSharp.TwoSum/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = [1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1];
+        int target = 11;
+
+        var result = TwoSumDict(nums, target);
+
+        System.Console.WriteLine($"{result[0]} & {result[1]}");
+
+
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int first = nums[i];
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i != j && nums[i] + nums[j] == target)
+                {
+                    return [i, j];
+                }
+            }
+        }
+        return [-1, -1];
+    }
+
+    public static int[] TwoSumDict(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(dict.ContainsKey(target - nums[i])){
+                return [i, dict[target - nums[i]]];
+            }
+            dict[nums[i]] = i;
+        }
+        return [];
+    }
+}
