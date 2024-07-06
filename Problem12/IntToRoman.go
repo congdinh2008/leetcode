b6084155df487cd72867ca606dd75099a317efc2
Cong Dinh <congdinh2012@hotmail.com>
2024-07-07 01:44:12 +0700

    Problem 12: Complete Go Solution

---
+++ Problem12/IntToRoman.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	num := 3999
+	fmt.Println(IntToRoman(num))
+}
+
+func IntToRoman(num int) string {
+	romans := map[int]string{
+		1000: "M",
+		900:  "CM",
+		500:  "D",
+		400:  "CD",
+		100:  "C",
+		90:   "XC",
+		50:   "L",
+		40:   "XL",
+		10:   "X",
+		9:    "IX",
+		5:    "V",
+		4:    "IV",
+		1:    "I",
+	}
+
+	keys := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+
+	var result strings.Builder
+
+	for _, k := range keys {
+		for num >= k {
+			result.WriteString(romans[k])
+			num -= k
+		}
+	}
+
+	return result.String()
+}
---
+++ Problem12/go.mod
@@ -0,0 +1,3 @@
+module congdinh.com/IntToRoman
+
+go 1.22.4
