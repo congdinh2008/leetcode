adadf8e5741aec2d4a82a26c263f81428763532c
Cong Dinh <congdinh2012@hotmail.com>
2024-07-07 14:28:08 +0700

    Problem 03: Complete C# Solution

---
+++ Problem03/CSharp.Problem03/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Security.AccessControl;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string s = "abceeabdcxbb";
+        System.Console.WriteLine(LengthOfLongestSubstring(s));
+    }
+
+    private static int LengthOfLongestSubstring(string s)
+    {
+        Dictionary<char, int> map = [];
+        int max = 0;
+        int left = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (map.ContainsKey(s[i]) && map[s[i]] >= left)
+            {
+                left = map[s[i]] + 1;
+            }
+            map[s[i]] = i;
+            int len = i - left + 1;
+            if (len > max)
+            {
+                max = len;
+            }
+        }
+
+        return max;
+    }
+}
