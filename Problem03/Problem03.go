bed22988a9e9712e28025b7f26b0ed85585aaacd
Cong Dinh <congdinh2012@hotmail.com>
2024-07-07 13:59:48 +0700

    Problem 03: Complete Go Solution

---
+++ Problem03/Problem03.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "abceeabdcxbb"
+	fmt.Println(lengthOfLongestSubstring(s))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	myMap := map[rune]int{}
+	maxLength := 0
+	left := 0
+
+	for right, char := range s {
+		if index, found := myMap[char]; found && index >= left {
+			left = index + 1
+		}
+		myMap[char] = right
+		currentLength := right - left + 1
+		if currentLength > maxLength {
+			maxLength = currentLength
+		}
+	}
+
+	return maxLength
+}
---
+++ Problem03/go.mod
@@ -0,0 +1,3 @@
+module congdinh.com/problem03
+
+go 1.22.4
